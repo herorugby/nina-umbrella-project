1f1716f9d72e9dd292361e726265e9453e0ce815
hirotomo <hero.rugby1015@gmail.com>
2021-01-24 11:30:36 +0900

    add coding

--- js/index.js
+++ js/index.js
@@ -1,26 +1,6 @@
 // modal menu
 
 
-// slideShow
-// $(function() {
-//   $(".main__img-slide-show").each(function() {
-//     let slides = $(this).find("img");
-//     let slideCount = slides.length;
-//     let currentIndex = 0;
-
-//     slides.eq(currentIndex).fadeIn(1000);
-
-//     setInterval(showNextSlide, 3000);
-
-//     function showNextSlide() {
-//       let nextIndex = (currentIndex + 1) % slideCount;
-//       slides.eq(currentIndex).fadeOut(1000);
-//       slides.eq(nextIndex).fadeIn(1000);
-//       currentIndex = nextIndex;
-//     }
-//   });
-// });
-
 // slide show with slick
 $(function() {
     $('.main__img-slide-show').slick( {
@@ -37,6 +17,27 @@ $(function() {
 });
 
 
+// gallery slide show pics wuth slick
+$(function() {
+    $('.main-items-slide').slick( {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        dots: false,
+        asNavFor: '.sub-items-slide'
+    });
+    $('.sub-items-slide').slick( {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+    });
+});
 
 // copy right get full year
 $(function() {
