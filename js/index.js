f23e02aa1c3e3627d6baca059e8f56c12471d3b2
hirotomo <hero.rugby1015@gmail.com>
2021-01-25 15:56:26 +0900

    some coding

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ $(function() {
         fade: true,
         swipe: false,
         pauseOnHover: false,
-        pauseOnFocus: false
+        pauseOnFocus: false,
     });
 });
 
@@ -20,21 +20,23 @@ $(function() {
 // gallery slide show pics wuth slick
 $(function() {
     $('.main-items-slide').slick( {
+        asNavFor: '.sub-items-slide',
+        infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3500,
         arrows: false,
         fade: true,
-        autoplay: true,
-        autoplaySpeed: 2500,
-        dots: false,
-        asNavFor: '.sub-items-slide'
     });
     $('.sub-items-slide').slick( {
-        slidesToShow: 2,
+        asNavFor: '.main-items-slide',
+        infinite: true,
+        slidesToShow: 3,
         slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: false,
-        centerMode: true,
+        accessibility: true,
+        speed: 400,
+        arrows: true,
         focusOnSelect: true,
     });
 });
