073734a5924b9fbf376cf4187999a1c495917d50
hirotomo <hero.rugby1015@gmail.com>
2021-02-13 23:33:23 +0900

    wp coding

--- archive.php
+++ archive.php
@@ -12,27 +12,90 @@
                     </div>
 
                     <div class="all-items-container">
-                        <?php
-                        $args = array(
-                            'post_type' => 'item',
-                        );
-                        $news_query = new WP_Query($args);
-                        if ($news_query->have_posts()) :
-                            while ($news_query->have_posts()) :
-                                $news_query->the_post();
-                        ?>
-                                <div class="all-items__details">
+                        <!-- <?php
+                                $args = array(
+                                    'post_type' => 'item',
+                                );
+                                $news_query = new WP_Query($args);
+                                if ($news_query->have_posts()) :
+                                    while ($news_query->have_posts()) :
+                                        $news_query->the_post(); ?>
+                                <a data-remodal-target="modal" class="all-items__details">
                                     <?php the_title(); ?>
                                     <div class="all-items__img">
                                         <?php the_post_thumbnail('thumbnail'); ?>
                                     </div>
-
-                                    <!-- <?php the_content(); ?> -->
+                                </a>
+                                <div class="remodal" data-remodal-id="modal">
+                                    <h1><?php the_title(); ?></h1>
+                                    <?php the_content(); ?>
+                                    <br>
+                                    <button data-remodal-action="cancel" class="remodal-cancel">閉じる</button>
+                                    <button data-remodal-action="confirm" class="remodal-confirm">アイテム一覧</button>
                                 </div>
 
                             <?php endwhile; ?>
                             <?php wp_reset_postdata(); ?>
+                        <?php endif; ?> -->
+
+
+                        <?php if (have_posts()) : ?>
+                            <?php while (have_posts()) : ?>
+                                <?php the_post(); ?>
+
+                                <a data-remodal-target="modal" class="all-items__details">
+                                    <?php the_title(); ?>
+                                    <div class="all-items__img">
+                                        <?php the_post_thumbnail('thumbnail'); ?>
+                                    </div>
+                                </a>
+                                <div class="remodal" data-remodal-id="modal">
+                                    <h1><?php the_title(); ?></h1>
+                                    <?php the_content(); ?>
+                                    <br>
+                                    <button data-remodal-action="cancel" class="remodal-cancel">閉じる</button>
+                                    <button data-remodal-action="confirm" class="remodal-confirm">アイテム一覧</button>
+                                </div>
+
+                            <?php endwhile; ?>
                         <?php endif; ?>
+
+
+
+                        <!-- <a data-remodal-target="modal" class="all-items__details">
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                            <picture class="all-items__img">
+                                <source media="(min-width: 960px)" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/test1.jpg">
+                                <source media="(min-width: 560px)" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/test1.jpg">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/test1.jpg" alt="test写真1">
+                            </picture>
+                        </a> -->
+
+                        <!-- <picture class="all-items__img">
+                                <source media="(min-width: 960px)" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/test1.jpg">
+                                <source media="(min-width: 560px)" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/test1.jpg">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/test1.jpg" alt="test写真1">
+                            </picture> -->
+                        <!-- <p>testtesttesttesttesttestてすとてすとてすとてすとてすとてすとてすとてすとテストテストテストテストテストテストテストテストテストテストテストテスト</p> -->
+
+
+
+
+                        <!-- <div class="remodal" data-remodal-id="modal">
+                            <button data-remodal-action="close" class="remodal-close"></button>
+                            <h1>ring01</h1>
+                            <picture class="all-items__img">
+                                <source media="(min-width: 960px)" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/test1.jpg">
+                                <source media="(min-width: 560px)" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/test1.jpg">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/test1.jpg" alt="test写真1">
+                            </picture>
+                            <p>testtesttesttesttesttestてすとてすとてすとてすとてすとてすとてすとてすとテストテストテストテストテストテストテストテストテストテストテストテスト</p>
+                            <?php the_content(); ?>
+                            <br>
+                            <button data-remodal-action="cancel" class="remodal-cancel">閉じる</button>
+                            <button data-remodal-action="confirm" class="remodal-confirm">アイテム一覧</button>
+                        </div> -->
+
 
                         <div class="all-items__details">
                             <picture class="all-items__img">
--- functions.php
+++ functions.php
@@ -35,8 +35,17 @@ add_filter('document_title_parts', 'nina_document_title_parts');
 function add_nina_styles()
 {
     wp_enqueue_style('nina_reset_style', get_template_directory_uri() . '/assets/stylesheets/reset.css');
+
     if (is_home()) {
+        // slick css cdn
+        wp_enqueue_style('slick_carousel', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css');
+        wp_enqueue_style('slick_carousel_theme', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css');
         wp_enqueue_style('nina_front_page_style', get_template_directory_uri() . '/assets/stylesheets/front-page.css');
+    }
+    if (is_archive()) {
+        // remodal css
+        wp_enqueue_style('remodal', get_template_directory_uri() . '/assets/stylesheets/remodal.css');
+        wp_enqueue_style('remodal_theme', get_template_directory_uri() . '/assets/stylesheets/remodal-default-theme.css');
     }
     if (is_archive() || is_page('workshop')) {
         wp_enqueue_style('nina_header_sub_style', get_template_directory_uri() . '/assets/stylesheets/other-page-header.css');
@@ -57,16 +66,19 @@ add_action('wp_enqueue_scripts', 'add_nina_styles');
  */
 function add_nina_scripts()
 {
-    // slick css cdn
-    wp_enqueue_style('slick-carousel', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css');
     // get jquery
     wp_enqueue_script('jquery');
     // slick jquery plugin cdn
-    wp_enqueue_script('slick-carousel', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js');
+    wp_enqueue_script('slick_carousel_js', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js');
+
+
     if (is_home()) {
-        wp_enqueue_script('nina-main-js', get_template_directory_uri() . '/assets/js/front-page.js', '', '', true);
-    } else if (is_archive() || is_page('workshop') || is_page('privacy') || is_page('trade')) {
-        wp_enqueue_script('nina-workshop-js', get_template_directory_uri() . '/assets/js/single-page.js', '', '', true);
+        wp_enqueue_script('nina_main_js', get_template_directory_uri() . '/assets/js/front-page.js', '', '', true);
+    } else if (is_archive()) {
+        wp_enqueue_script('nina_remodal_js', get_template_directory_uri() . '/assets/js/remodal.min.js');
+        wp_enqueue_script('nina_isolate_js', get_template_directory_uri() . '/assets/js/single-page.js', '', '', true);
+    } else if (is_page('workshop') || is_page('privacy') || is_page('trade')) {
+        wp_enqueue_script('nina_isolate_js', get_template_directory_uri() . '/assets/js/single-page.js', '', '', true);
     }
 }
 add_action('wp_enqueue_scripts', 'add_nina_scripts');
