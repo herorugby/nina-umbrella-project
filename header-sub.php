8fcbd52fd1f9ae2a3325ce24314a7e714f32e882
herorugby <hero.rugby1015@gmail.com>
2021-03-17 08:46:46 +0900

    new css codes

--- header-sub.php
+++ header-sub.php
@@ -9,7 +9,8 @@
 
     <!-- google fonts notoserifjp lora -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@600&family=Noto+Serif+JP:wght@600&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Lora:wght@600&family=Noto+Serif+JP:wght@600&display=swap" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Kosugi+Maru&family=Noto+Sans&display=swap" rel="stylesheet">
 
 
     <!-- read fontsawesome -->
--- header.php
+++ header.php
@@ -9,13 +9,13 @@
 
     <!-- google fonts notoserifjp lora dancingscript -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Lora:wght@600&family=Noto+Serif+JP:wght@600&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Lora:wght@600&family=Noto+Serif+JP:wght@600&display=swap" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Kosugi+Maru&family=Noto+Sans&display=swap" rel="stylesheet">
 
     <!-- read fontsawesome -->
     <script src="https://kit.fontawesome.com/72a93cd7e4.js" crossorigin="anonymous"></script>
 
-    <?php
-    wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -39,12 +39,11 @@
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/header__img-slide-show2.jpg" alt="トップページのスライド画像">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/header__img-slide-show3.jpg" alt="トップページのスライド画像">
         </div>
-        <!-- <span id="mouse"></span> -->
 
         <nav id="gnav" class="container gnav__container">
             <ul class="gnav__list">
                 <li class="gnav__list__item"><a href="<?php echo esc_url(home_url('/')); ?>">HOME & About</a></li>
-                <li class="gnav__list__item"><a href="<?php echo esc_url(home_url('/workshop')); ?>">Workshop</a></li>
+                <li class="gnav__list__item"><a href="<?php echo esc_url(home_url('/workshop/')); ?>">Workshop</a></li>
                 <li class="gnav__list__item"><a href="<?php echo get_post_type_archive_link('item'); ?>">Items</a></li>
             </ul>
         </nav>
